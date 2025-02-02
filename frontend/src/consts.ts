2ebf02b5b53322992a9873a4dbbbcdfe1edf6702
Caleb Elizondo <calebelizondo@tamu.edu>
2025-02-02 15:13:24 -0600

    moved auth to router

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,1 +1,2 @@
-export const AUTH_STORAGE_KEY = "auth_token"
+export const AUTH_STORAGE_KEY = "auth_token"; 
+export const LOGIN_PATH = "/login";
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/Login.vue'
 import Dashboard from '@/views/Dashboard.vue'
+import { AUTH_STORAGE_KEY, LOGIN_PATH } from '@/consts';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,39 @@ const router = createRouter({
       component: Login
     }
   ],
-})
+}); 
+
+async function verifyAuthToken(token: string) {
+  try {
+    const response = await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${token}`);
+    const data = await response.json();
+
+    if (data.error) {
+      return null;
+    }
+
+    return data;
+  } catch (error) {
+    return null;
+  }
+}
+
+router.beforeEach(async (to, format, next) => {
+  const token = localStorage.getItem(AUTH_STORAGE_KEY);
+
+  if (to.path === LOGIN_PATH) next();
+
+  if (!token) {
+    next(LOGIN_PATH);
+  }else {
+    const userInfo = await verifyAuthToken(token);
+    if (!userInfo){
+      next(LOGIN_PATH);
+    } else {
+      next();
+    }
+  }
+});
+
 
 export default router
